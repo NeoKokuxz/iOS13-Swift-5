2ef60368e4888f529742d4aad1168394dce1992e
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2020-01-21 00:19:15 +0800

    UIPicker update

--- 3.Networking, JSON, APIs and Core locations/ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
+++ 3.Networking, JSON, APIs and Core locations/ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var currencyDisplay: UILabel!
-    @IBOutlet weak var currencyUnit: UILabel!
+    @IBOutlet weak var currencyValue: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var pickerLabel: UIPickerView!
+    
+    let coinManager = CoinManager() //Initialize coinManager
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        pickerLabel.dataSource = self //Set picker data source to current ViewController
+        pickerLabel.delegate = self //Set picker delegate to current ViewController
     }
-
-
 }
 
+//MARK: - UIPickerViewDataSource
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 //return number of columns in picker
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count //return number of rows in picker
+    }
+}
+
+//MARK: - UIPickerViewDelegate
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return coinManager.currencyArray[row] //Check current currency label in the array
+    }
+}
